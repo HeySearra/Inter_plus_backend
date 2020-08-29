396e7719be398ff2f4fbaf38a1c79e5b628474b5
HeySearra <48943939+HeySearra@users.noreply.github.com>
2020-08-29 23:26:30 +0800

    url去斜杠

--- inter_plus_backend/inter_plus_backend/urls.py
+++ inter_plus_backend/inter_plus_backend/urls.py
@@ -5,38 +5,39 @@ from user.views import *
 from exercise.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('upload/', include([
-        path('pic/', UploadImage.as_view()),
+        path('pic', UploadImage.as_view()),
 
     ])),
     path('user/', include([
         path('register', Register.as_view()),
         path('login', Login.as_view()),
         path('user_info', UserInfo.as_view()),
-        path('user_account/', ChangePassword.as_view()),
-        path('identity/', GetIdentity.as_view()),
-        path('post_teacher/', ApplyForTeacher.as_view()),
+        path('user_account', ChangePassword.as_view()),
+        path('identity', GetIdentity.as_view()),
+        path('post_teacher', ApplyForTeacher.as_view()),
     ])),
     path('exercise/', include([
-        path('course_list/', CourseExerciseList.as_view()),
-        path('del_exercise/', DelExercise.as_view()),
-        path('question/', include([
-            path('info/', GetQuestionInfo.as_view()),
-            path('choices/', GetChoices.as_view()),
+        path('course_list', CourseExerciseList.as_view()),
+        path('del_exercise', DelExercise.as_view()),
+        path('question', include([
+            path('info', GetQuestionInfo.as_view()),
+            path('choices', GetChoices.as_view()),
         ]))
     ])),
+    path('subject/list', GetSubjectList.as_view()),
     path('course/', include([
-        path('subject_list/', GetSubjectList.as_view()),
-        path('list/', GetCourseList.as_view()),
-        path('user_list/', GetUserCourseList.as_view()),
+        path('subject_list', GetSubjectList.as_view()),
+        path('list', GetCourseList.as_view()),
+        path('user_list', GetUserCourseList.as_view()),
         path('note_list', GetCourseNoteList.as_view()),
-        path('user_note_list/', GetCourseUserNoteList.as_view()),
-        path('info/', CourseInfo.as_view()),
-        path('join/', JoinCourse.as_view()),
-        path('like/', LikeCourse.as_view()),
-        path('class_info/', ClassInfo.as_view()),
+        path('user_note_list', GetCourseUserNoteList.as_view()),
+        path('info', CourseInfo.as_view()),
+        path('join', JoinCourse.as_view()),
+        path('like', LikeCourse.as_view()),
+        path('class_info', ClassInfo.as_view()),
     ])),
-    path('video/info/', VideoInfo.as_view()),
-    path('note/info/', NoteInfo.as_view()),
+    path('video/info', VideoInfo.as_view()),
+    path('note/info', NoteInfo.as_view()),
 ]
